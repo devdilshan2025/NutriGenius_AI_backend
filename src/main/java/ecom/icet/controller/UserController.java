47b43c2e47d61830632b317c0726eac4fc7a1bfe
devdilshan2025 <dsbandara4@gmail.com>
2026-04-18 19:11:48 +0530

    create user controller

---
+++ src/main/java/ecom/icet/controller/UserController.java
@@ -0,0 +1,26 @@
+package ecom.icet.controller;
+
+import ecom.icet.model.dto.UserDto;
+import ecom.icet.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor // Constructor injection එක ලේසි කරන්න ලොම්බොක් පාවිච්චි කරනවා
+@CrossOrigin
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody UserDto userDto) {
+        userService.registerUser(userDto);
+        return "User Registered Successfully!";
+    }
+
+    @GetMapping("/personalized-advice")
+    public String getPersonalizedAdvice(@RequestParam String email) {
+        return userService.getAiAdviceForUser(email);
+    }
+}
