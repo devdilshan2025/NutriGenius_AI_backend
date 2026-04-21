2b1c605e7e1cc596373a3486c2624f3300db398b
devdilshan2025 <dsbandara4@gmail.com>
2026-04-21 10:04:30 +0530

    set the crossorigin

--- src/main/java/ecom/icet/controller/AiController.java
+++ src/main/java/ecom/icet/controller/AiController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/ai")
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 public class AiController {
 
     private final AiCoachService aiCoachService;
