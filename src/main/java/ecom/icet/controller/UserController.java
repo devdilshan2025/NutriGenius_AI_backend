7e14cb2f6c27671efc5003cd79cd976fd7039647
devdilshan2025 <dsbandara4@gmail.com>
2026-04-21 10:04:51 +0530

    Update  the crossorigin

--- src/main/java/ecom/icet/controller/UserController.java
+++ src/main/java/ecom/icet/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor // Constructor injection එක ලේසි කරන්න ලොම්බොක් පාවිච්චි කරනවා
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     private final UserService userService;
